482b61df65c351c5820519ba957e82ef74ff4c5d
rmchale <ryan.mchale@gmail.com>
2021-05-06 17:29:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.git
 curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.yml -o .gitpod.yml
 curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.pycharm.sh -o .gitpod.pycharm.sh
 chmod 755 .gitpod.pycharm.sh
+curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.bootstrap.sh -o .gitpod.bootstrap.sh
+chmod 755 .gitpod.bootstrap.sh
