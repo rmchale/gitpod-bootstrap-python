82e9f3e4b43b2a1ccc91727b594307862932e57c
rmchale <ryan.mchale@gmail.com>
2021-05-05 21:10:25 -0500

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.Dockerfile -o .gitpod.Dockerfile
+curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.yml -o .gitpod.yml
+curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.pycharm.sh -o .gitpod.pycharm.sh
