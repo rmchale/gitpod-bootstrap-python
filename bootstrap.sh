1508684ff0ff07432aa9b1bb4c809c8e3c5554c1
rmchale <ryan.mchale@gmail.com>
2021-05-06 17:15:37 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,4 @@
 curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.Dockerfile -o .gitpod.Dockerfile
 curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.yml -o .gitpod.yml
 curl https://raw.githubusercontent.com/rmchale/gitpod-bootstrap-python/main/.gitpod.pycharm.sh -o .gitpod.pycharm.sh
+chmod 755 .gitpod.pycharm.sh
